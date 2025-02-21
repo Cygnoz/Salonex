ff9f8c961ca610cf9ec309cee911f380e29e4fcd
SanalCygnoz <sanalk2003@gmail.com>
2025-02-21 17:16:32 +0530

    foc

--- src/pages/pos/SelectedCustomerModal.tsx
+++ src/pages/pos/SelectedCustomerModal.tsx
@@ -1,53 +1,58 @@
 import { useEffect, useState } from "react";
-import ChevronDown from "../../assets/icons/CheveronDown";
-import Modal from "../../Components/modal/Modal";
 import SearchBar from "../../Components/SearchBar";
 import defaultCustomerImage from "../../assets/Images/Rectangle 5558.png";
-// import { endponits } from "../../Services/apiEndpoints";
-// import useApi from "../../Hooks/useApi";
+import useApi from "../../Hooks/useApi";
 import Button from "../../Components/Button";
+import { endpoints } from "../../Services//apiEndpoints";
+import CheveronDown from "../../assets/icons/CheveronDown";
+import Modal from "../../Components/modal/Modal";
 
 type Props = { onButtonClick: (data: string) => void; };
 
 function SelectCustomerModal({ onButtonClick }: Props) {
-  const [customerData, 
-    // setCustomerData
-  ] = useState<any>([]);
+  const [customerData, setCustomerData] = useState<any>([]);
   const [selectedCustomer, setSelectedCustomer] = useState<any>(null);
-  // const { request: AllCustomers } = useApi("get", 5002);
+  const { request: AllCustomers } = useApi("get", 5002);
   const [isModalOpen, setModalOpen] = useState(false);
   const [searchValue, setSearchValue] = useState<string>("");
 
-  const openModal = () => {
-    setModalOpen(true);
-  };
+
 
   const closeModal = () => {
     setModalOpen(false);
   };
 
   const fetchAllCustomers = async () => {
-    // try {
-    //   const url = `${endponits}`; getallcustomers
-    //   const { response, error } = await AllCustomers(url);
-    //   if (!error && response) {
-    //     const customers = response.data;
-    //     setCustomerData(customers);
-    //     if (customers.length > 0) {
-    //       setSelectedCustomer(customers[0]);
-    //     }
-    //   } else {
-    //     console.log(error, "all customers error");
-    //   }
-    // } catch (error) {
-    //   console.error("Error fetching accounts:", error);
-    // }
+    try {
+      const url = `${endpoints.GET_ALL_CUSTOMER}`;
+      const { response, error } = await AllCustomers(url);
+
+      if (!error && response) {
+        const customers = response.data.filter(
+          (customer: { status?: string }) => customer.status !== "Inactive"
+        );
+
+        setCustomerData(customers);
+        if (customers.length > 0) {
+          setSelectedCustomer(customers[0]);
+        }
+      } else {
+        console.log(error, "all customers error");
+      }
+    } catch (error) {
+      console.error("Error fetching customers:", error);
+    }
   };
+
 
   useEffect(() => {
     fetchAllCustomers();
   }, []);
 
+  const openModal = () => {
+    fetchAllCustomers()
+    setModalOpen(true);
+  };
 
   useEffect(() => {
     fetchAllCustomers();
@@ -64,12 +69,12 @@ function SelectCustomerModal({ onButtonClick }: Props) {
   return (
     <>
       <div
-        className="relative bg-white flex rounded-xl px-3 py-[6px] text-sm items-center"
+        className="relative border-2 border-slate-200 flex rounded-md px-2 py-1 text-sm items-center"
         onClick={openModal}
       >
         <span className="me-5 text-[#818894]">Select Customer</span>
         <div className="pointer-events-none inset-y-0 right-1 flex items-center">
-          <ChevronDown color="#495160" />
+          <CheveronDown color="#495160" />
         </div>
       </div>
 
@@ -79,7 +84,7 @@ function SelectCustomerModal({ onButtonClick }: Props) {
         onClose={closeModal}
       >
         <div className="flex justify-between items-center mb-2">
-          <span className="text-textColor text-sm font-bold">
+          <span className="text-[#303F58] text-sm font-bold">
             Customer Details
           </span>
           <p className="text-3xl font-light cursor-pointer" onClick={closeModal}>
@@ -99,8 +104,8 @@ function SelectCustomerModal({ onButtonClick }: Props) {
               <div
                 key={customer.id}
                 className={`bg-[#F5F2EE] rounded-lg p-4 flex flex-col h-32 items-center justify-center cursor-pointer ${selectedCustomer?._id === customer._id
-                    ? "border-2 border-[#948B7C]"
-                    : ""
+                  ? "border-2 border-[#948B7C]"
+                  : ""
                   }`}
                 onClick={() => setSelectedCustomer(customer)}
               >
@@ -126,15 +131,15 @@ function SelectCustomerModal({ onButtonClick }: Props) {
                   <div className="mt-3 bg-white p-5 rounded-lg">
                     <div className="flex justify-between">
                       <div>
-                        <p className="text-dropdownText text-xs font-semibold">Customer Name</p>
-                        <p className="text-textColor text-sm font-semibold">{selectedCustomer?.customerDisplayName}</p>
+                        <p className="text-[#4B5C79] text-xs font-semibold">Customer Name</p>
+                        <p className="text-[#303F58] text-sm font-semibold">{selectedCustomer?.customerDisplayName}</p>
                       </div>
                       <div>
                         <img src={selectedCustomer?.customerProfile || defaultCustomerImage} className="w-10 h-10 rounded-full" alt="" />
                       </div>
                     </div>
-                    <p className="text-dropdownText text-xs font-semibold mt-3">Billing Address</p>
-                    <p className="text-textColor text-sm">
+                    <p className="text-[#4B5C79] text-xs font-semibold mt-3">Billing Address</p>
+                    <p className="text-[#303F58] text-sm">
                       {selectedCustomer?.billingAddressLine1 || ""}{""}
                       {selectedCustomer?.billingAddressLine2 || ""}{" "}
                       {selectedCustomer?.billingCity || ""}{" "}
@@ -142,9 +147,9 @@ function SelectCustomerModal({ onButtonClick }: Props) {
                       {selectedCustomer?.billingState || ""}{" "}
                       {selectedCustomer?.billingPinCode || ""}
                     </p>
-                    <p className="text-dropdownText text-xs font-semibold mt-3">Shipping Address</p>
+                    <p className="text-[#4B5C79] text-xs font-semibold mt-3">Shipping Address</p>
 
-                    <p className="text-textColor text-sm ">{selectedCustomer?.shippingAddress1} {selectedCustomer?.shippingAddress2} {selectedCustomer?.shippingCity} {selectedCustomer?.shippingCountry} {selectedCustomer?.shippingState} {selectedCustomer?.shippingPinCode}</p>
+                    <p className="text-[#303F58] text-sm ">{selectedCustomer?.shippingAddress1} {selectedCustomer?.shippingAddress2} {selectedCustomer?.shippingCity} {selectedCustomer?.shippingCountry} {selectedCustomer?.shippingState} {selectedCustomer?.shippingPinCode}</p>
                   </div>
                 </div>
               </>
